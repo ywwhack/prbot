c7367ce46e20de6e26b5c7a9f9f01e7257cbdb9a
ywwhack <yww7289@163.com>
2017-06-12 14:49:14 +0800

    only process opened pull_request hook

--- app.js
+++ app.js
@@ -7,9 +7,22 @@ const body = require('koa-better-body')
 const app = new Koa()
 const router = koaRouter()
 
+const PR_EVENT = 'pull_request'
+const OPEN_PR_ACTION = 'opened'
+
 router.post('/hooks', async (ctx, next) => {
-  console.log(ctx.request.headers['X-GitHub-Event'])
-  console.log(ctx.request.fields)
+  const receiveEvent = ctx.request.headers['x-github-event']
+  // 只处理 pull_request，其余 hooks 直接返回
+  if (receiveEvent === PR_EVENT) {
+    const payload = ctx.request.fields
+    if (payload.action === OPEN_PR_ACTION) {
+      const prUrl = payload.pull_request && payload.pull_request.html_url
+      if (prUrl) {
+        console.log(prUrl)
+        // send this url to wechat
+      }
+    }
+  }
   ctx.body = 'ok'
 })
 
