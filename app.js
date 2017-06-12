63ea8bfa100ac150061974585636be36dde01b68
ywwhack <yww7289@163.com>
2017-06-12 12:29:20 +0800

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const Koa = require('koa')
+const koaRouter = require('koa-router')
+const serve = require('koa-static')
+const convert = require('koa-convert')
+const body = require('koa-better-body')
+
+const app = new Koa()
+const router = koaRouter()
+
+router.post('/hooks', async (ctx, next) => {
+  console.log(this.request.fields)
+  ctx.body = 'ok'
+})
+
+app.use(convert(body()))
+app.use(serve('.'))
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log('listending on port 3000')
+})
