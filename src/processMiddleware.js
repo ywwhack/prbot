bed4f619ae25db977dd069fb40d2eadbd0718cb8
ywwhack <yww7289@163.com>
2017-06-14 16:56:57 +0800

    deal with pull_request_review_comment && issue-comment event

--- src/processMiddleware.js
+++ src/processMiddleware.js
@@ -1,5 +1,5 @@
 const { filehelperMsgQueue, dtMsgQueue, userMsgQueueMap } = require('./bootstrap/messageQueues')
-const { PULL_REQUEST, PULL_REQUEST_REVIEW } = require('./constants/events')
+const { PULL_REQUEST, PULL_REQUEST_REVIEW, PULL_REQUEST_REVIEW_COMMENT, ISSUE_COMMENT } = require('./constants/events')
 
 function processPullRequest (eventName, payload, wechatBot) {
   if (eventName === PULL_REQUEST) {
@@ -12,18 +12,55 @@ function processPullRequest (eventName, payload, wechatBot) {
 
 function processPullRequestReview (eventName, payload, wechatBot) {
   if (eventName === PULL_REQUEST_REVIEW) {
-    const { review, pull_request } = payload
-    const body = review.body
-    const notifierMsgQueue = userMsgQueueMap[pull_request.user.login]
-    // 被通知的人必须要存在机械人的好友中
-    // 并且 pull_request_review 值不为空
-    if (body && notifierMsgQueue) {
-      notifierMsgQueue.send(`${review.user.login}：${body}`)
-    }
+    const { review, pull_request, repository } = payload
+    send({
+      from: review,
+      to: pull_request,
+      wechatBot,
+      repository
+    })
+  }
+}
+
+function processPullRequestReviewComment (eventName, payload, wechatBot) {
+  if (eventName === PULL_REQUEST_REVIEW_COMMENT) {
+    const { comment, pull_request, repository } = payload
+    send({
+      from: comment,
+      to: pull_request,
+      wechatBot,
+      repository
+    })
+  }
+}
+
+function processIssueComment (eventName, payload, wechatBot) {
+  if (eventName === ISSUE_COMMENT) {
+    const { issue, comment, repository } = payload
+    send({
+      from: comment,
+      to: issue,
+      wechatBot,
+      repository
+    })
+  }
+}
+
+// util functions
+function send ({ from, to, wechatBot, message, repository }) {
+  const body = from.body
+  const notifierMsgQueue = userMsgQueueMap[to.user.login]
+  // 被通知的人必须要存在机械人的好友中
+  // 并且 pull_request_review 值不为空
+  if (body && notifierMsgQueue) {
+    const msg = message || `${repository.name} 有一条来自 ${from.user.login} 的新评论：${ body.length > 20 ? body.slice(0, 17) + '...' : body }\n点击查看 ${from.html_url}`
+    notifierMsgQueue.send(msg)
   }
 }
 
 module.exports = [
   [PULL_REQUEST, processPullRequest],
-  [PULL_REQUEST_REVIEW, processPullRequestReview]
+  [PULL_REQUEST_REVIEW, processPullRequestReview],
+  [PULL_REQUEST_REVIEW_COMMENT, processPullRequestReviewComment],
+  [ISSUE_COMMENT, processIssueComment]
 ]
