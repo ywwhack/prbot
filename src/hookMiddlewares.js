4bfdbf2ab9a220e862a075480d6a0c3afb67a9ea
ywwhack <yww7289@163.com>
2017-06-18 14:50:45 +0800

    add pull_request_review && issue_comment process

--- src/hookMiddlewares.js
+++ src/hookMiddlewares.js
@@ -1,4 +1,4 @@
-const { PULL_REQUEST, PULL_REQUEST_REVIEW, PULL_REQUEST_REVIEW_COMMENT, ISSUE_COMMENT } = require('./constants/events')
+const { PULL_REQUEST, PULL_REQUEST_REVIEW, ISSUE_COMMENT } = require('./constants/events')
 const { binding } = require('./bootstrap/binding')
 const MessageQueue = require('./lib/MessageQueue')
 
@@ -17,9 +17,33 @@ function processPullRequest (payload, wechatBot) {
   }
 }
 
+function processPullRequestReview (payload, wechatBot) {
+  const { review, pull_request } = payload
+  const { state, user: { login: proposer } } = review
+  const owner = pull_request.user.login
+  // 只有 state 不是 pedding，并且不是自己的评论才会转发
+  if (state !== 'pedding' && proposer !== owner) {
+    MessageQueue.send(
+      proposer + ' has ' + state + ' on ' + payload.repository.name 
+      + ', see: ' + review.html_url,
+    owner)
+  }
+}
+
+function processIssueComment (payload, wechatBot) {
+  const { comment, issue } = payload
+  const { title, user: { login: owner } } = issue
+  const proposer = comment.user.login
+  if (proposer !== owner) {
+    MessageQueue.send(
+      proposer + ' has comment on ' + payload.repository.name + '\'s issue'
+      + ', see: ' + comment.html_url,
+    owner)
+  }
+}
+
 module.exports = [
-  [PULL_REQUEST, processPullRequest]
-  // [PULL_REQUEST_REVIEW, processPullRequestReview],
-  // [PULL_REQUEST_REVIEW_COMMENT, processPullRequestReviewComment],
-  // [ISSUE_COMMENT, processIssueComment]
+  [PULL_REQUEST, processPullRequest],
+  [PULL_REQUEST_REVIEW, processPullRequestReview],
+  [ISSUE_COMMENT, processIssueComment]
 ]
